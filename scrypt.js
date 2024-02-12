f764e612c34264f4850db1e67c4fd817f9716645
2Tucho <albergromero@gmail.com>
2024-02-12 20:22:41 +0100

    Ejercicio listo

---
+++ scrypt.js
@@ -0,0 +1,19 @@
+//Chauchau
+
+const chauchat = () => {
+    for (let i = 1; i < 8; i++){
+        const cargador = "pium!";
+        let numeroAlAzar = Math.floor(Math.random() * 100); // Un número entre 1 y 100 al azar
+        
+        if (numeroAlAzar > 80) {
+            console.log(cargador);            
+            } else {
+            console.log("Illo, me he quedao pillá!");
+            }  
+      if (i%3 === 0){
+                console.log('');
+              }
+    }
+};
+
+chauchat();
